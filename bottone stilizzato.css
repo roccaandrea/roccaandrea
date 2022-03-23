15edcaa6ebc7f56ce1fe2d6421e565342caf44d5
roccaandrea <95033740+roccaandrea@users.noreply.github.com>
2022-03-23 18:18:41 +0100

    Create bottone stilizzato.css

---
+++ bottone stilizzato.css
@@ -0,0 +1,18 @@
+<html lang="en">
+    <head>
+      <meta charset="UTF-8" /> <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+    <title>Document</title>
+ <style>
+    #myDIV { background-color: white; width: 200px; height: 200px; padding: 10px; }
+ </style>
+ </head>
+ <body> <div id="myDIV"> <p id="out">Times</p>
+ 
+ <button type="button" onclick="count()">Push Me</button> 
+ </div>
+ <script> let i = 0; let start = Date.now(); let CosoClick = 0;
+ const myInterval = setInterval(pippo,1000); let contatore = 0; function count() { document.getElementById("out").innerHTML += " " + (Date.now() - start) + " ms"; CosoClick++; document.getElementById("myDIV").style.backgroundColor = "red"; } 
+  </script> 
+   </body>
+ </html>
